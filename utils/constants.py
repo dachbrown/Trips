77ec491c49cb7a38324789ff9415a7d7490b6959
SayehBayat <syhbyt@gmail.com>
2021-11-25 11:34:04 -0500

    constants

---
+++ utils/constants.py
@@ -0,0 +1,53 @@
+"""
+Useful constants
+"""
+
+UID = 'uid'
+LATITUDE = 'Latitude'
+LONGITUDE = 'Longitude'
+ORIGIN = 'origin'
+DESTINATION = 'destination'
+ORIGIN_LAT = 'TSLat'
+ORIGIN_LNG = 'TSLong'
+ORIGIN_TIME = 'TStime'
+DESTINATION_LAT = 'TELat'
+DESTINATION_LNG = 'TELong'
+DESTINATION_TIME = 'TEtime'
+DATETIME = 'Date_Time'
+DURATION = 'Duration'
+SPEED = 'Vehicle_Speed'
+ACC = 'Acceleration'
+JERK = 'Jerk'
+EVENT = 'Event_Type'
+DIFFERENCE = 'Difference'
+ODOMETER = 'Odometer_Reading'
+DISTANCE = 'Trip_Distance'
+TRAVELLED_DISTANCE = "Travelled_Dist"
+STRAIGHT_DISTANCE = "Straight_Dist"
+LIMIT = 'Speed_Limit'
+NIGHT = 'Night_Trip'
+HOUR = 'Hour'
+BUSINESS_DAY = 'Business_Day'
+MORNING_RUSH = 'Morning_Rush'
+EVENING_RUSH = 'Evening_Rush'
+DIST_GRP = 'Distance_Group'
+HB = 'HARD_BREAKING_MESSAGE'
+HCB = 'HARD_CORE_BRAKING_MESSAGE'
+HA = 'HARD_ACCELERATION_MESSAGE'
+COUNT_OS = 'Count_Over_Speeding'
+DUR_OS = 'Dur_Over_Speeding'
+COUNT_US = 'Count_Under_Speeding'
+DUR_US = 'Dur_Under_Speeding'
+FLOW = 'flow'
+TILE_ID = 'tile_ID'
+CLUSTER = 'cluster'
+LEAVING_DATETIME = 'leaving_datetime'
+FREQUENCY = "freq"
+PROBABILITY = "prob"
+TOTAL_FREQ = "T_freq"
+COUNT = "count"
+TEMP = "tmp"
+PROPORTION = "prop"
+TOT_OUTFLOW = "tot_outflow"
+PRECISION_LEVELS = ["Year", "Month", "Day", "Hour", "Minute", "Second", "year", "month", "day", "hour", "minute",
+                    "second"]
---
+++ utils/utils.py
@@ -0,0 +1,24 @@
+import numpy as np
+
+
+def getDistance(loc1, loc2):
+    """
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
+
+    All args must be of equal length.
+
+    """
+    lat1, lon1 = loc1
+    lat2, lon2 = loc2
+
+    # convert to radians
+    lon1, lat1, lon2, lat2 = map(np.radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = np.sin(dlat/2.0)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon/2.0)**2
+    c = 2 * np.arcsin(np.sqrt(a))
+    km = 6367 * c
+    return km
